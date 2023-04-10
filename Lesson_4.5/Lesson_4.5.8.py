6fd1e79bf04b6c2ecbc94f3628263b810b999365
Victor Smirnov <125829316+Victor-Smirnoff@users.noreply.github.com>
2023-04-10 20:17:21 +0300

    Create Lesson_4.5.8.py

---
+++ Lesson_4.5/Lesson_4.5.8.py
@@ -0,0 +1,10 @@
+from zipfile import ZipFile
+
+
+def extract_this(zip_name, *args):
+    with ZipFile(zip_name) as zip_file:
+        if len(args) == 0:
+            zip_file.extractall()
+        else:
+            for file in args:
+                zip_file.extract(file)
